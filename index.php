46c2ec5fc570bf29f79b00e84b85b96d72540d1e
HsuanHsuan <100360425+a1093306@users.noreply.github.com>
2022-03-14 23:52:54 +0800

    homework2

--- index.php
+++ index.php
@@ -1,15 +1,70 @@
 <html>
+
 <head>
+    <title>墾丁三日遊</title>
+    <link rel="icon" href=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2wI5VQBs26xZ8N0pqvpd_7Yjfp86Stj-PUQ&usqp=CAU" type="image/x-icon" />
 </head>
+
 <body>
     <?php
-    $i=10;
-    $j=20;
-    echo"<b>Hello</b> <h1>PHP</h1>";
-    echo "<br/>";
-    echo$i+$j;
-    echo"<br/>";
-    echo date("Y,m,d");
+        echo "<body bgcolor='#20EDE2'>";
     ?>
+    <font face="標楷體">
+        <center><h1>墾丁三日遊</h1></center>
+    </font>
+    <font size='4'face="標楷體">
+        <h3>第一天</h3>
+        時段&nbsp&nbsp&nbsp&nbsp行程<br/>
+        <font color="red">早上</font>&nbsp&nbsp&nbsp&nbsp由高雄火車站或小港機場到墾丁。→午餐。(請參考高雄往墾丁的客運)<br/>
+        <font color="red">中午</font>&nbsp&nbsp&nbsp&nbsp小灣戲水→青年活動中心→情人灘拾貝、觀落日→晚餐至恆春覓食。<br/>
+        <font color="red">晚上</font>&nbsp&nbsp&nbsp&nbsp墾丁大街踩街→找店預約第二天水上活動→小灣觀星→回民宿裡睡覺。<br/>
+        <h3>第二天</h3>
+        時段&nbsp&nbsp&nbsp&nbsp行程<br/>
+        <font color="red">早上</font>&nbsp&nbsp&nbsp&nbsp早餐→水上活動(浮潛、乘坐香蕉船、水上摩托車…等)→午餐。<br/>
+        <font color="red">中午</font>&nbsp&nbsp&nbsp&nbsp找店承租一天的摩托車(至明天中午)<br/>
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp往西半島出發→南灣→瓊麻館→後壁湖碼頭→貓鼻頭→白砂→關山飛來石→關山日落→晚餐至恆春覓食。<br/>
+        <font color="red">晚上</font>&nbsp&nbsp&nbsp&nbsp恆春→出火→回墾丁大街補踩街→鵝鑾鼻燈塔夜遊→龍磐觀星→回民宿做好夢…<br/>
+        <h3>第三天</h3>
+        時段&nbsp&nbsp&nbsp&nbsp行程<br/>
+        <font color="red">早上</font>&nbsp&nbsp&nbsp&nbsp早餐→東半島之旅→船帆石→貝殼砂展示館→路過燈塔→聯勤→龍磐草原→風吹砂→港口吊橋→路過出火→恆春→墾丁<br/>
+        <font color="red">中午</font>&nbsp&nbsp&nbsp&nbsp歸還摩托車→午餐吃過飯，打包行李趕路回家。<br/>
+    </font>
+    <br/>
+    <br/>
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <a href="http://uukt.com.tw/attraction/4"> <img src="https://img.uukt.com.tw/view/4/1080/201611092225213f7pB.jpg" border='2'width="42%"></a>
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <a href="http://uukt.com.tw/attraction/41"> <img src="https://img.uukt.com.tw/view/41/1080/20191017115753dar4w.jpg" border='2' width="43%"></a><br/>
+    <br/>
+    <form>
+        <font  face="Times New Roman" size='5'>
+            Please input your Name:&nbsp&nbsp<input type="text" name="uName" placeholder="name name name" required><br/>
+            Please input your Email:&nbsp&nbsp<input type="email" name="email" require><br/>
+            Please input your Phonenumber:&nbsp&nbsp<input type="tel" name="phone" require><br/>
+            Please input your Gender:&nbsp&nbsp<input type="radio" name="male">Male<input type="radio" name="femal">Femal<input type="radio" name="none">N/A<br/>
+            Please input your Food Preference:&nbsp&nbsp<input type="checkbox" name="meat">肉<input type="checkbox" name="vegetetable">蔬菜<input type="checkbox" name="fish">魚<input type="checkbox" name="rice">米飯<input type="checkbox" name="noodle">麵食<input type="checkbox" name="seafood">海鮮<br/>
+            Please choose your T-shirt size:&nbsp&nbsp
+            <select name="size">
+                <option>S</option>
+                <option>M</option>
+                <option>L</option>
+                <option>XL</option>
+            </select>
+            <br/>
+            Please input T-shirt Color:&nbsp&nbsp<input type="color" name="PWD"><br/>
+            Please input your Birthday:&nbsp&nbsp<input type="date" name="birthday"><br/>
+            Please input Ticket Number:&nbsp&nbsp<input type="number" name="ticket"><br/>    
+        </font>
+        <input type="submit">
+    </form>
+    <center><h2>T-shirt size對照表</h2></center>
+    <center><table border='1'width="70%">
+        <tr>&nbsp&nbsp<th></th><th>S</th><th>M</th><th>L</th><th>XL</th></tr>
+        <tr><td><center>肩寬</center></td><td><center>52</center></td><td><center>54.5</center></td><td><center>57</center></td><td><center>59</center></td></tr>
+        <tr><td><center>胸寬</center></td><td><center>51</center></td><td><center>53.5</center></td><td><center>56.5</center></td><td><center>59.5</center></td></tr>
+        <tr><td><center>袖長</center></td><td><center>19.5</center></td><td><center>20.5</center></td><td><center>21.5</center></td><td><center>22</center></td></tr>
+        <tr><td><center>衣長</center></td><td><center>68.5</center></td><td><center>71</center></td><td><center>73.5</center></td><td><center>76</center></td></tr>
+    </table></center>
 </body>
+
 </html>
